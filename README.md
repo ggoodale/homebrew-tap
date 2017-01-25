fb80ea8fe648d1c950a7473d7b319b60c2157e3b
Grant Goodale <grant@convoy.com>
2017-01-25 10:08:18 -0800

    First cut at a plv8 tap

---
+++ README.md
@@ -0,0 +1,15 @@
+# homebrew-tap
+[brew](https://github.com/mxcl/homebrew/) repository for random formulae.
+
+## Usage  
+Tap into the repository using the tap command:
+
+    brew tap ggoodale/tap
+
+    brew install ggoodale/tap/<project> <project-options>
+
+## Available Formulae  
+* [**plv8**](https://github.com/plv8/plv8) - V8 Engine Javascript Procedural Language add-on for PostgreSQL
+
+## Issues  
+Something does not work or is out of date? Create an [issue](https://github.com/ggoodale/homebrew-tap/issues) or drop me an [email](https://github.com/ggoodale).
---
+++ plv8.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Plv8 < Formula
+  desc 'V8 Engine Javascript Procedural Language add-on for PostgreSQL'
+  homepage 'https://github.com/plv8/plv8'
+  version '1.5.5'
+  url 'https://github.com/plv8/plv8/archive/v1.5.5.tar.gz'
+  sha1 '19e97ccd14343401701acacf8cfa4df457ed24de'
+ 
+  head 'https://github.com/plv8/plv8', :using => :git
+ 
+  depends_on 'v8' # and postgres
+  depends_on :postgresql
+ 
+  def install
+    ENV.prepend 'PATH', Formula.factory('postgresql').bin, ':'
+    system 'pg_config' # ensure postgres installed
+    system "make"
+    system "make install"
+  end
+ 
+  def test
+    ENV.prepend 'PATH', Formula.factory('postgresql').bin, ':'
+    system "make installcheck"
+  end
+end
